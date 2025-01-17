bbff50889e60dea96f5364e8848d917dee9c606b
renan <renanmakoto@outlook.com>
2025-01-17 10:22:42 -0500

    Adding Python file

---
+++ lab1/lab1.py
@@ -0,0 +1,12 @@
+#Sample Coding Questions 01 Week 01
+#First Name: Renan; Last Name: Yoshida Avelan
+labArray = [1, 4, 7, 9]
+a = 1
+b = 2
+c = 3
+d = 4
+e = ((a - ((b ** c) // d)) + (a % c))
+temperature = 32.6
+print(f'The temperature today is: {temperature:3.f} degrees Celsiu')
+userAge = int(input('Input your age: '))
+print(f'Now showing the shop intems filtered by age: {userAge}')
