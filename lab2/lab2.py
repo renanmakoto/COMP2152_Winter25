008543a05227ac774be2991dc52f1e760cf46405
renan <renanmakoto@outlook.com>
2025-01-17 11:08:22 -0500

    lab2

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,5 +1,15 @@
 import random
 def number_guessing_game():
     targetNumber = random.randint(1, 100)
+    play = input('Do you want to play guessing number game? y/n').strip().lower()
+
+
+    attempts = 0
+    max_attempts = 10
+    while attempts < max_attempts:
+        try:
+            return True
+        except:
+            print('')
     play = input()
     return True
