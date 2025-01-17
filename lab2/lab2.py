938b9f39d70c179287242f7b7ccc168c77a5b4cf
renan <renanmakoto@outlook.com>
2025-01-17 13:31:36 -0500

    Adding the folder to the proper place

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -2,14 +2,26 @@ import random
 def number_guessing_game():
     targetNumber = random.randint(1, 100)
     play = input('Do you want to play guessing number game? y/n').strip().lower()
-
-
     attempts = 0
     max_attempts = 10
     while attempts < max_attempts:
         try:
-            return True
-        except:
-            print('')
-    play = input()
-    return True
+            userGuess = int(input('Enter your guess: '))
+            attempts += 1
+            if userGuess < targetNumber:
+                if abs(userGuess - targetNumber) <= 5:
+                    print('Too low, but close. Try again.')
+                else:
+                    print('Too low. Try again.')
+            elif userGuess > targetNumber:
+                if abs(userGuess - targetNumber) <= 5:
+                    print('Too high, but close. Try again.')
+                else:
+                    print('Too high')
+            else:
+                print(f'Congratulations you have guessed it in {attempts} attempt.')
+                return
+        except ValueError:
+            print('Invalid input! Please, enter a number between 1 and 100')
+    print(f'Game over! The target number was {targetNumber}.')   
+number_guessing_game()
